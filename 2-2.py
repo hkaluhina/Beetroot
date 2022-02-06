1dca604e426528f3d5d48adf939312926e250ef0
hkaluhina <91496587+hkaluhina@users.noreply.github.com>
2022-02-06 21:37:26 +0200

    Add files via upload

---
+++ 2-2.py
@@ -0,0 +1,9 @@
+# выведет фразу и попросит введ имени
+name = input("What is your name? ")
+#выведет привет, (введенное имя)
+print("Hi,", name)
+# сеп покажет каким будет разделитель
+print(name, 'Hi!', sep=' dear ')
+# выведет  с !!!!!
+print('Friend', end='!!!!!')
+print('Hello')
