7e4561de55ea71b53ac377fcf242719a0f390510
hkaluhina <91496587+hkaluhina@users.noreply.github.com>
2022-02-06 21:38:25 +0200

    Add files via upload

---
+++ Task2-1.py
@@ -0,0 +1,11 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#hi
+print('hello, world')
+hello, world
+print('Hello', 'world', sep=',')
+Hello,world
+print('Hello', end='!!!')
+Hello!!!
+print('world')
+world
